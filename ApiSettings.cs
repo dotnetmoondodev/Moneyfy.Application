e8a75ef2433f5b71ff5cec1f1410915bdd0e4b7c
Hector Gomez <moon.do.dev@gmail.com>
2025-06-12 11:39:12 -0500

    added logger and db configuration data, finally removed the logfile configuration

--- ApiSettings.cs
+++ ApiSettings.cs
@@ -1,11 +1,32 @@
+using Microsoft.Extensions.Logging;
+
 namespace Application;
+
+public static class LoggerSettings
+{
+    public static string Default => "Logging:LogLevel:Default";
+
+    public static LogLevel GetLogLevel( string? logLevel )
+    {
+        return logLevel switch
+        {
+            "Trace" => LogLevel.Trace,
+            "Debug" => LogLevel.Debug,
+            "Information" => LogLevel.Information,
+            "Warning" => LogLevel.Warning,
+            "Error" => LogLevel.Error,
+            "Critical" => LogLevel.Critical,
+            _ => LogLevel.Information
+        };
+    }
+}
 
 public static class ApiSettings
 {
     public static string Key => $"{nameof( ApiSettings )}:Key";
     public static string Issuer => $"{nameof( ApiSettings )}:Issuer";
     public static string Audience => $"{nameof( ApiSettings )}:Audience";
-    public static string LogFilePath => $"{nameof( ApiSettings )}:LogFilePath";
+    public static string DBConnection => $"{nameof( ApiSettings )}:DBConnection";
 
     public static string BuildJwtKey( string? secret )
     {
