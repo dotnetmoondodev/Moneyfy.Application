facb233a3cbaf6d28d1426733814ab2b9006cb95
Hector Gomez <moon.do.dev@gmail.com>
2025-07-28 13:15:52 -0500

    added config params to use jaeger tracing

--- Settings/WebApiSettings.cs
+++ Settings/WebApiSettings.cs
@@ -4,10 +4,14 @@ public class WebApiSettings: BaseSettings
 {
     public string? DBConnection { get; init; }
     public string? KeyVaultName { get; init; }
+    public string? JaegerHost { get; init; }
+    public int JaegerPort { get; init; }
 
     public override bool DataIsValid()
     {
         return base.DataIsValid() &&
-            !string.IsNullOrEmpty( DBConnection );
+            !string.IsNullOrEmpty( DBConnection ) &&
+            !string.IsNullOrEmpty( JaegerHost ) &&
+            ( JaegerPort > 0 );
     }
 }
