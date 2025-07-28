ee7505cb8789a4c29c62415ef39f2f1528179a13
Hector Gomez <moon.do.dev@gmail.com>
2025-07-28 13:33:31 -0500

    Revert "added config params to use jaeger tracing"
    This reverts commit facb233a3cbaf6d28d1426733814ab2b9006cb95.

--- Settings/WebApiSettings.cs
+++ Settings/WebApiSettings.cs
@@ -4,14 +4,10 @@ public class WebApiSettings: BaseSettings
 {
     public string? DBConnection { get; init; }
     public string? KeyVaultName { get; init; }
-    public string? JaegerHost { get; init; }
-    public int JaegerPort { get; init; }
 
     public override bool DataIsValid()
     {
         return base.DataIsValid() &&
-            !string.IsNullOrEmpty( DBConnection ) &&
-            !string.IsNullOrEmpty( JaegerHost ) &&
-            ( JaegerPort > 0 );
+            !string.IsNullOrEmpty( DBConnection );
     }
 }
